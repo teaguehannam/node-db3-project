efbc82696df4cfdf3324d0aafd2c1e6b7d860797
Teague <teaguehannam@gmail.com>
2020-04-16 16:10:02 -0600

    Fixed join table query

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,18 +14,23 @@ module.exports ={
 function find() {
     return db('schemes');
 };
+
 function findById(id){
     return db ('schemes').where({id}).first();
 };
-function findSteps(id){
-    return 
-        db('steps as s' ).join('schemes as sc', 'sc.id', 's.scheme_id')
-            .select('s.id', 'sc.scheme_name', 's.instructions')
-            .where({ scheme_id: id });
+
+function findSteps(id) { 
+    return db('steps')
+        .join("schemes", "schemes.id", "=", "scheme_id")
+        .select("schemes.id", "scheme_name", "step_number", "instructions")
+        .where({'scheme_id' : id})
+        .orderBy('step_number')
 };
+
 function add (dataToInsert){
     return db('schemes').insert(dataToInsert);
 };
+
 function update(change, id){
     return db('schemes')
             .where({id})
